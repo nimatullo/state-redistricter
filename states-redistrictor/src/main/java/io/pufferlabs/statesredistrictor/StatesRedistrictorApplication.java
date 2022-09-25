cdeabec7917a611af11065739f7ad4ec3dac1ec1
NKinalis <nikolaskinalis@hotmail.com>
2022-09-25 11:39:11 -0400

    refactored workspace into client-server

---
+++ states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/StatesRedistrictorApplication.java
@@ -0,0 +1,13 @@
+package io.pufferlabs.statesredistrictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatesRedistrictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatesRedistrictorApplication.class, args);
+	}
+
+}
