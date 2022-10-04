3745d723ebc400868a66b2b0b330961cbda4acc2
Sherzod <shermanjones99@gmail.com>
2022-10-04 15:59:33 -0400

    moved logic to service class

--- client/src/components/state/StateOverview.jsx
+++ client/src/components/state/StateOverview.jsx
@@ -4,37 +4,7 @@ import React from "react";
 import stateService from "../../services/stateService";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-const RADIAN = Math.PI / 180;
-
 ChartJS.register(ArcElement, Tooltip, Legend);
-
-// const renderCustomizedLabel = ({
-//   cx,
-//   cy,
-//   midAngle,
-//   innerRadius,
-//   outerRadius,
-//   percent,
-//   index,
-// }) => {
-//   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-//   const x = cx + radius * Math.cos(-midAngle * RADIAN);
-//   const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-//   console.log(index);
-
-//   return (
-//     <text
-//       x={x}
-//       y={y}
-//       fill="white"
-//       textAnchor={x > cx ? "start" : "end"}
-//       dominantBaseline="central"
-//     >
-//       {`${(percent * 100).toFixed(0)}%`}
-//     </text>
-//   );
-// };
 
 const StateOverview = ({ state }) => {
   const [stateData, setStateData] = React.useState({});
@@ -44,19 +14,19 @@ const StateOverview = ({ state }) => {
     setStateData(data.percentages);
   }, []);
 
-  const getData = (data) => {
+  const convertToChartJSDataset = (data) => {
     return {
       labels: data.map((d) => d.title),
       datasets: [
         {
           data: data.map((d) => `${Number(d.percentage).toFixed(1)}`),
-          backgroundColor: data.map((d) => randomColor()),
+          backgroundColor: data.map((d) => generateColor()),
         },
       ],
     };
   };
 
-  const randomColor = () => {
+  const generateColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   };
 
@@ -65,7 +35,7 @@ const StateOverview = ({ state }) => {
       <Heading size="2xl" mb="1em">
         Overview
       </Heading>
-      <Tabs variant={"enclosed-colored"} colorScheme={"gray"}>
+      <Tabs variant={"soft-rounded"} colorScheme={"gray"}>
         <TabList>
           <Tab>State Demographics</Tab>
           <Tab>Ensemble Summary Comparison</Tab>
@@ -73,21 +43,27 @@ const StateOverview = ({ state }) => {
 
         <TabPanels>
           <TabPanel>
-            <Heading size="xl">Demographic breakdown</Heading>
+            <Heading size="xl" my="16px">
+              Demographic breakdown
+            </Heading>
             <Flex justifyContent={"space-between"}>
               {stateData.length > 0 &&
                 stateData.map((data) => {
                   return (
                     <Box>
                       <Heading size="md">{Object.keys(data)[0]} (%)</Heading>
-                      <Pie data={getData(Object.values(data)[0])} />
+                      <Pie
+                        data={convertToChartJSDataset(Object.values(data)[0])}
+                      />
                     </Box>
                   );
                 })}
             </Flex>
           </TabPanel>
           <TabPanel>
-            <Heading size="xl">Generated ensemble summary</Heading>
+            <Heading size="xl" my="16px">
+              Generated ensemble summary
+            </Heading>
             <Flex justifyContent={"space-between"}>
               <Box>
                 <Heading size="lg" fontWeight={"medium"}>
