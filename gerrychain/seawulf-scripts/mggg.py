dc69e55bd87a0f37f9574375738a547a06f52023
Mark Moawad <msmoawad99@gmail.com>
2022-11-04 19:04:37 -0400

    need to create initial graph, then run partition

--- gerrychain/seawulf-scripts/mggg.py
+++ gerrychain/seawulf-scripts/mggg.py
@@ -3,7 +3,7 @@ from gerrychain.updaters import Tally, cut_edges
 import os
 
 # grab NC_VTD.json from shapefiles/processed/north carolina/NC_VTD.json
-NC_path = os.path.join(os.getcwd(), "shapefiles/processed/north carolina")
-path = os.path.join(NC_path, "NC_VTD.json")
-graph = Graph.from_json(path)
+NC_path = os.path.join(os.getcwd(), "shapefiles/processed/florida")
+path = os.path.join(NC_path, "FL_VTDs.shp")
+graph = Graph.from_file(path)
 print(graph.nodes[0])
