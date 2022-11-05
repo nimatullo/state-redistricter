e49514171b21cbe73b36dadb608fae35a5ea04e2
Mark Moawad <msmoawad99@gmail.com>
2022-11-05 13:25:54 -0400

    testing out init partition with given PA data from gerryChain site

--- gerrychain/seawulf-scripts/mggg.py
+++ gerrychain/seawulf-scripts/mggg.py
@@ -2,8 +2,20 @@ from gerrychain import Graph, Partition, Election
 from gerrychain.updaters import Tally, cut_edges
 import os
 
-# grab NC_VTD.json from shapefiles/processed/north carolina/NC_VTD.json
-NC_path = os.path.join(os.getcwd(), "shapefiles/processed/florida")
-path = os.path.join(NC_path, "FL_VTDs.shp")
-graph = Graph.from_file(path)
-print(graph.nodes[0])
+graph_path = os.path.join(os.getcwd(), "PA_test.json")
+graph = Graph.from_json(graph_path)
+election = Election("Sen16", {"Dem": "SEN16D", "Rep": "SEN16R"})
+
+initial_partition = Partition(
+	graph,
+	assignment="CD_2011",
+	updaters={
+		"population": Tally("TOTPOP", alias="population"),
+		"cut_edges": cut_edges,
+		"Sen16": election
+	}
+)
+
+for district, pop in initial_partition["population"].items():
+	print("District {} has population {}".format(district, pop))
+print(initial_partition.graph.nodes[0])
