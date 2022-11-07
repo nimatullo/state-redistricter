2f251a21f31927e87069a487810a05c3ff0d34dc
Mark Moawad <msmoawad99@gmail.com>
2022-11-06 20:47:09 -0500

    produced markov chain

--- gerrychain/seawulf-scripts/mggg.py
+++ gerrychain/seawulf-scripts/mggg.py
@@ -1,21 +1,37 @@
-from gerrychain import Graph, Partition, Election
+import geopandas as gpd
+from gerrychain import Graph, Partition, Election, MarkovChain
+from gerrychain.constraints import single_flip_contiguous 
+from gerrychain.proposals import propose_random_flip
+from gerrychain.accept import always_accept
 from gerrychain.updaters import Tally, cut_edges
 import os
 
-graph_path = os.path.join(os.getcwd(), "PA_test.json")
-graph = Graph.from_json(graph_path)
-election = Election("Sen16", {"Dem": "SEN16D", "Rep": "SEN16R"})
+graph_path = os.path.join(os.getcwd(), "shapefiles", "processed", "north carolina", "precinct_level_w_election.shp")
+# df = gpd.read_file(graph_path)
+# print(df.columns)
+
+graph = Graph.from_file(graph_path, ignore_errors=True)
+election = Election("SEN16", {"Democratic": "SEN16D", "Republican": "SEN16R"})
 
 initial_partition = Partition(
 	graph,
-	assignment="CD_2011",
+	assignment="CD",
 	updaters={
 		"population": Tally("TOTPOP", alias="population"),
 		"cut_edges": cut_edges,
-		"Sen16": election
+		"SEN16": election
 	}
 )
 
-for district, pop in initial_partition["population"].items():
-	print("District {} has population {}".format(district, pop))
-print(initial_partition.graph.nodes[0])
+chain = MarkovChain(
+	proposal=propose_random_flip,
+	constraints=[single_flip_contiguous],
+	accept=always_accept,
+	initial_state=initial_partition,
+	total_steps=10
+)
+
+# print one partition
+for partition in chain:
+	print(partition.assignment)
+
