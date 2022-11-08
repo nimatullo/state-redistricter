64604c104a262a189136788b2058e2fc52fd3617
NKinalis <nikolaskinalis@hotmail.com>
2022-11-08 14:05:50 -0500

    fixed dummy db data format

--- states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/model/State.java
+++ states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/model/State.java
@@ -1,8 +1,6 @@
 package io.pufferlabs.statesredistrictor.model;
 
 import io.pufferlabs.statesredistrictor.enums.StateAbbreviation;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
@@ -12,7 +10,6 @@ import java.util.List;
 @Document(collection = "states")
 @Data
 public class State {
-    @Id
     private String name;
     private StateAbbreviation abbreviation;
     private StateShape stateShape;
