b180b0da4114a2bcfc7f752791f894fff52ddd31
alex <alexperez52@gmail.com>
2022-11-08 18:31:08 -0500

    Code cleanup

--- states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/controller/StatesController.java
+++ states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/controller/StatesController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/states")
@@ -16,28 +15,19 @@ public class StatesController {
 
     private final StatesService statesService;
 
-    private final StateRepository stateRepository;
 
     @Autowired
     public StatesController(StatesService statesService, StateRepository stateRepository) {
         this.statesService = statesService;
-        this.stateRepository = stateRepository;
     }
 
-    //DEPRECATED
-//    @GetMapping("/{state}/shape")
-//    public ResponseEntity<?> getGeoJsonForState(@PathVariable String state) {
-//        return statesService.readGeoJsonFromDisk(state);
-//    }
 
-    //get all states
+
     @GetMapping
     public ResponseEntity<?> getStates() {
         return ResponseEntity.ok(statesService.getStates());
     }
 
-
-    // get state by name
     @GetMapping("/{stateName}")
     public ResponseEntity<?> getStateByName(@PathVariable String stateName) {
         State state = statesService.getStateByName(stateName);
