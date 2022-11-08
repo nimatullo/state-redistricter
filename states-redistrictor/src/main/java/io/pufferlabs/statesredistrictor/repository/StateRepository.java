bb3e73af6236db3b35e77405bab096f1ad9ca926
alex <alexperez52@gmail.com>
2022-11-07 21:02:36 -0500

    Added some dummy state repo functions

--- states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/repository/StateRepository.java
+++ states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/repository/StateRepository.java
@@ -1,12 +1,20 @@
 package io.pufferlabs.statesredistrictor.repository;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import com.mongodb.client.MongoClient;
+import io.pufferlabs.statesredistrictor.model.Overview;
+import io.pufferlabs.statesredistrictor.model.State;
 
 
-public interface StateRepository extends MongoRepository<State, String> {
+public interface StateRepository extends MongoRepository<State, String>, QuerydslPredicateExecutor<State> {
 
-    
+    @Query("{ 'abbreviation' : ?0 }")
+    Overview findByNameAndIncludeStateOverviewOnly(String stateName, Enum PlanType);
+
+    @Query("{ 'abbreviation' : ?0 }")
+    State findByNameIncludeStateShapeOnly(String stateName);
     
 }
     
