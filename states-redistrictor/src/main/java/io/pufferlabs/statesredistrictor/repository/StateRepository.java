c2f8ba7a91b10c9025b3c35780eb65f17af222bf
alex <alexperez52@gmail.com>
2022-11-07 21:17:47 -0500

    Changes to repository - 1 function

--- states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/repository/StateRepository.java
+++ states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/repository/StateRepository.java
@@ -4,17 +4,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import io.pufferlabs.statesredistrictor.model.Overview;
 import io.pufferlabs.statesredistrictor.model.State;
 
 
 public interface StateRepository extends MongoRepository<State, String>, QuerydslPredicateExecutor<State> {
 
-    @Query("{ 'abbreviation' : ?0 }")
-    Overview findByNameAndIncludeStateOverviewOnly(String stateName, Enum PlanType);
-
-    @Query("{ 'abbreviation' : ?0 }")
-    State findByNameIncludeStateShapeOnly(String stateName);
+    @Query("{ 'stateName' : ?0 }")
+    State findByAbbreviation(String stateName);
     
 }
     
