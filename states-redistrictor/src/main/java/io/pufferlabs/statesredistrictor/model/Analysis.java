0e4c66e7820283ff8cba3dfdbca0d585f1969ab1
alex <alexperez52@gmail.com>
2022-11-11 19:42:46 -0500

    Remove extra collections

--- states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/model/Analysis.java
+++ states-redistrictor/src/main/java/io/pufferlabs/statesredistrictor/model/Analysis.java
@@ -3,13 +3,11 @@ package io.pufferlabs.statesredistrictor.model;
 import io.pufferlabs.statesredistrictor.enums.Race;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
-@Document(collection = "analyzes")
 public class Analysis {
     @Id
     private String state;
