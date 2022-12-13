20ea6f8b3be6f72e5834bad9f4fd75ce4c1e60d3
nimatullo <nimatullo@users.noreply.github.com>
2022-12-13 22:45:12 +0000

    Prettified Code!

--- gerrychain/data/processed/db processing/parse_raw_data.js
+++ gerrychain/data/processed/db processing/parse_raw_data.js
@@ -1,7 +1,11 @@
 let uniqueDistrictPlanIdCounter = 0;
 let fs = require("fs");
 let rawSMDData = JSON.parse(fs.readFileSync("../utah/data.json"));
-let rawMMDData = JSON.parse(fs.readFileSync("/home/niko/development/416/gerrychain/seawulf-scripts/state_summary_data/ut_data.json"));
+let rawMMDData = JSON.parse(
+  fs.readFileSync(
+    "/home/niko/development/416/gerrychain/seawulf-scripts/state_summary_data/ut_data.json"
+  )
+);
 let state = "Utah";
 let abbreviation = "UT";
 let rawMMDSubEnsembles = rawMMDData.all_data;
